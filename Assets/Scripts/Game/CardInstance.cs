df35e8fc4b7f13c272b5ee1021de021024b2b3a4
KOSMOGOR <mortalmortal2015@mail.ru>
2026-05-04 00:35:50 +0300

    add: DOTween, some player methods

--- Assets/Scripts/Game/CardInstance.cs
+++ Assets/Scripts/Game/CardInstance.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class CardInstance : MonoBehaviour
 {
     public CardData cardData;
     public int remainInPlay;
+
+    SpriteRenderer sprite;
+
+    void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetCardActive(bool isActive) {
+        sprite.enabled = isActive;
+    }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,14 +1,19 @@
 using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public int currentOxygenTank = 20;
     public int oxygenTanks = 2;
+    public Transform handCenter;
+    public float handDeltaDistance;
+    public CardInstance cardInstancePrefab;
 
-    List<CardInstance> deck = new();
-    List<CardInstance> hand = new();
-    List<CardInstance> discard = new();
+    List<CardInstance> deck = new(); // 0 - left card
+    List<CardInstance> hand = new(); // 0 - top of the deck
+    List<CardInstance> discard = new(); // 0 - bottom of discard
 
     public CardInstance selectedCard;
     public CardInstance cardInPlay;
@@ -34,11 +39,35 @@ public class Player : MonoBehaviour
     public void DiscardCardInPlay() {
         if (cardInPlay != null) {
             discard.Add(cardInPlay);
+            cardInPlay.SetCardActive(true);
             cardInPlay = null;
         }
     }
 
     public void AddPlayerGameEffects(CardInstance card) {
         card.cardData.playerGameEffectsOnResolve.ForEach(ge => GameEffectInstance.CreateAndAdd(ge, playerGameEffects));
+    }
+
+    void RepositionCards() {
+        if (hand.Count == 0) return;
+        float startX = handCenter.position.x - (hand.Count - 1) * handDeltaDistance / 2;
+        for (int i = 0; i < hand.Count; i++) hand[i].transform.DOMove(new(startX + handDeltaDistance * i, handCenter.position.y, handCenter.position.z), 1).SetEase(Ease.OutCubic);
+    }
+
+    void ShuffleDiscardToDeck() {
+        deck.AddRange(discard);
+        discard.Clear();
+        deck.OrderBy(_ => Random.value);
+    }
+
+    public void DrawCard() {
+        if (deck.Count == 0) ShuffleDiscardToDeck();
+        if (deck.Count > 0) {
+            CardInstance card = deck[0];
+            deck.RemoveAt(0);
+            hand.Add(card);
+            card.SetCardActive(true);
+            RepositionCards();
+        }
     }
 }
