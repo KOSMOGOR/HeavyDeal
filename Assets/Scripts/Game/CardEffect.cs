680a7bfa1d8e9e196f7cda39ab5f2a96275f9c79
SfedBro <fegalinka@mail.ru>
2026-05-12 00:02:40 +0300

    branch switch commit

--- Assets/Scripts/Game/CardEffect.cs
+++ Assets/Scripts/Game/CardEffect.cs
@@ -5,6 +5,10 @@ public abstract class CardEffect
 {
     public virtual bool RequiresTarget => false;
 
+    public virtual void OnDraw(Player player) {}
     public virtual void OnPlay(Player player) {}
+    public virtual void OnActive(Player player) {}
     public virtual void OnResolve(Player player) {}
+    public virtual void OnDiscard(Player player) {}
+    public virtual void OnRemove(Player player) {}
 }
